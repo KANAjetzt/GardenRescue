4ff53b3d91cc124b5e613fe9151978a5498a0a8a
Kai Roth <kai@kana.jetzt>
2022-07-10 23:40:10 +0200

    🔨 plant texture change

--- scenes/Plant.gd
+++ scenes/Plant.gd
@@ -7,42 +7,56 @@ export(Texture) var fruit_texture
 export(Texture) var dead_texture
 export(String, "Store", "Shack") var location = "Store"
 export(Dictionary) var stages = {
-	"seed": 0,
-	"seedling": 1,
-	"sprouting": 2,
-	"fruit": 3,
+	"seed": 1,
+	"seedling": 4,
+	"sprouting": 5,
+	"fruit": 6,
 }
 export(String, "seed", "seedling", "sprouting", "fruit") var current_stage = "seed"
 
+var stages_with_texture = null
 var age = 0
-var textures = []
-var current_texture_index = 1
-var current_texture = null
+
 
 onready var GameWorld = get_node("/root/GameWorld")
 
 func _ready():
 	GameWorld.connect("new_day", self, "_on_new_day")
-	
-	textures = [dead_texture, seed_texture, seedling_texture, sprouting_texture, fruit_texture]
-	current_texture = textures[current_texture_index]
-	age = stages[current_stage]
+		
+	stages_with_texture = {
+	"seed": {
+		"days": stages.seed,
+		"texture": seed_texture
+	},
+	"seedling": {
+		"days": stages.seedling,
+		"texture": seedling_texture
+	},
+	"sprouting": {
+		"days": stages.sprouting,
+		"texture": sprouting_texture
+	},
+	"fruit": {
+		"days": stages.fruit,
+		"texture": fruit_texture
+	},
+}
 
 func _on_new_day(day):
 	# Plant gets older
 	age += 1
+	print(age)
 	
 	# Check if max stage
-	if(age >= stages.fruit):
+	if(age > stages.fruit):
 		return
 	
 	# Check if there is a new stage reached
 	for stage_key in stages.keys():
 		var stage = stages[stage_key]
 		if(stage == age):
+			print(stage_key)
 			# update stage
 			current_stage = stage
 			# update texture
-			current_texture_index += 1
-			current_texture = textures[current_texture_index]
-			$CurrentStageTexture.texture = current_texture
+			$CurrentStageTexture.texture = stages_with_texture[stage_key].texture
