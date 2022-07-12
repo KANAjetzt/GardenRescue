b2d330869c3a5a29cf08d485da59af2a9adab7ab
Kai Roth <kai@kana.jetzt>
2022-07-12 12:42:20 +0200

    ✔ made plant scene more flexible

--- scenes/Plant.gd
+++ scenes/Plant.gd
@@ -1,61 +1,32 @@
 extends Node2D
 
-export(Texture) var seed_texture
-export(Texture) var seedling_texture
-export(Texture) var sprouting_texture
-export(Texture) var fruit_texture
 export(Texture) var dead_texture
-export(String, "Store", "Shack") var location = "Store"
-export(Dictionary) var stages = {
-	"seed": 1,
-	"seedling": 4,
-	"sprouting": 5,
-	"fruit": 6,
-}
-export(String, "seed", "seedling", "sprouting", "fruit") var current_stage = "seed"
+export(Array, int) var stages_day = []
+export(Array, Texture) var stages_texture = []
 
-var stages_with_texture = null
 var age = 0
+var stage_index = 0
 var grid_position = Vector2(0, 0)
-
 
 onready var GameWorld = get_node("/root/GameWorld")
 
 func _ready():
 	GameWorld.connect("new_day", self, "_on_new_day")
-		
-	stages_with_texture = {
-	"seed": {
-		"days": stages.seed,
-		"texture": seed_texture
-	},
-	"seedling": {
-		"days": stages.seedling,
-		"texture": seedling_texture
-	},
-	"sprouting": {
-		"days": stages.sprouting,
-		"texture": sprouting_texture
-	},
-	"fruit": {
-		"days": stages.fruit,
-		"texture": fruit_texture
-	},
-}
+	
+	$CurrentStageTexture.texture =  stages_texture[stage_index]
 
 func _on_new_day(day):
 	# Plant gets older
 	age += 1
 	
 	# Check if max stage
-	if(age > stages.fruit):
+	if(stage_index + 1 == stages_day.size()):
 		return
 	
 	# Check if there is a new stage reached
-	for stage_key in stages.keys():
-		var stage = stages[stage_key]
-		if(stage == age):
+	for day in stages_day:
+		if(day == age):
 			# update stage
-			current_stage = stage
+			stage_index = stages_day.find(day)
 			# update texture
-			$CurrentStageTexture.texture = stages_with_texture[stage_key].texture
+			$CurrentStageTexture.texture = stages_texture[stage_index]
